138114ca5b5526a02e023a278f27b4d096989359
Ernesto Vázquez <vazquez.ernesto@gmail.com>
2025-11-03 20:20:58 -0300

    Fix routing: Switch to HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import "./styles/App.css";
 
 // Importa tus páginas
