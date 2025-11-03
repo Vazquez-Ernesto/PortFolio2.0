2336edea14dfaf899c747f366ce5f373b1ccfecf
Ernesto Vázquez <vazquez.ernesto@gmail.com>
2025-11-03 10:01:39 -0300

    Deploy portfolio to main domain

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Ajuste para GitHub Pages: cambia el nombre si tu repo es distinto
-  base: '/PortFolio2.0/',
-  build: {  
+  base: '/', 
+  build: {
     outDir: 'docs'
   },
   server: {
